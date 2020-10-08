c94b9dce7379168321d7701c2cf4c4b860f5b26f
suint <kellymi@udel.edu>
2020-10-08 01:29:25 -0400

    added list

--- aloeha/src/App.tsx
+++ aloeha/src/App.tsx
@@ -2,25 +2,60 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const data = {
+  plants: [
+    {name: "Daisy", id: 1},
+    {name: "Rose", id: 2}
+  ]
+}
+
+type PlantProps = { //idk what their actual props are
+  name: string,
+  id: number
+};
+
+let selectedPlants: Array<PlantProps> = [];
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PlantLookup />
+      <SelectedPlants />
     </div>
   );
+}
+
+class SelectedPlants extends React.Component {
+  render() {
+    return (<p>Hi</p>)
+  }
+}
+
+class PlantLookup extends React.Component<{}, {plants: Array<PlantProps>}>  {
+  constructor(props: Array<PlantProps>) {
+    super(props);
+    this.state = {plants: data.plants}
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.plants.map((item, key) => {
+          return <Plant name={item.name} id={item.id} />
+        })}
+      </ul>
+    );
+  }
+}
+
+const Plant = (props: PlantProps) => {
+  return (
+    <li onClick={() => updateSelected(props.name, props.id)}>{props.name}: {props.id}</li>
+  )
+}
+
+function updateSelected(plantName: string, plantId: number) {
+  selectedPlants.push({name: plantName, id: plantId});
 }
 
 export default App;
