d9334ef1541d678b7a9f3310e811e6751b70abb0
suint <kellymi@udel.edu>
2020-11-02 01:22:29 -0500

    search function filters data

--- aloeha/src/components/Select.tsx
+++ aloeha/src/components/Select.tsx
@@ -1,9 +1,17 @@
+import { findAllByAltText } from "@testing-library/react";
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import placeholder from "./img/reference pictures/Screen Shot 2020-10-18 at 10.24.02 PM.png"
+import './select.css';
 
 const data: Array<PlantProps> = [
     {name: "Daisy", id: 1},
-    {name: "Rose", id: 2}
+    {name: "Rose", id: 2},
+    {name: "Sunflower", id: 3},
+    {name: "Dandelion", id: 4},
+    {name: "Iris", id: 5},
+    {name: "Tulip", id: 6},
+    {name: "Hydrangea", id: 7},
 ]
 
 type PlantProps = {
@@ -25,6 +33,7 @@ const Select = () => {
     )
 }
   
+// at some later point, we will need to lift the state here (and all other components) into app.tsx
 class PlantBox extends React.Component<{}, boxState> {
     constructor(props: any) {
         super(props);
@@ -37,8 +46,8 @@ class PlantBox extends React.Component<{}, boxState> {
 
     showResults = (data: PlantProps[]) => {
         this.setState({
-        searchPlants: data,
-        selectedPlants: []
+            searchPlants: data,
+            selectedPlants: []
         })
     }
 
@@ -48,11 +57,22 @@ class PlantBox extends React.Component<{}, boxState> {
         const isInSearchResults = searchPlants.some(result => result.id === clickedPlant.id);
 
         this.setState({
-            // check which list clicked plant startss in and remove if it's in that one, add if it's not
+            // check which list clicked plant starts in and remove if it's in that one, add if it's not
             searchPlants: isInSearchResults ? searchPlants.filter(i => i.id  !== clickedPlant.id) : [...searchPlants, clickedPlant],
             selectedPlants: isInSearchResults ? [...selectedPlants, clickedPlant] : selectedPlants.filter(i => i.id !== clickedPlant.id)
         });
 }
+    handleSearch = () => {
+        let input = document.getElementById("search-input") as HTMLInputElement;
+        if (input.value) {
+            let searchText = input.value;
+            console.log(searchText)
+            this.setState({searchPlants: data.filter((plant) => plant.name.includes(searchText))});
+        } else {
+            this.setState({searchPlants: data});
+        }
+        console.log(this.state);
+    }
 
 
 componentDidMount() {
@@ -61,13 +81,19 @@ componentDidMount() {
 
 render() {
     return (
-        <div className="plantbox">
-            <div id="search-plants">
+        <div id="plantbox">
+            <h1>Plant Select</h1>
+            <div id="search-plants" className="plants">
+                <p>Search for plants you would like to place in your garden.</p>
+                <p>Once you have found your plant, click on its name to add it to your list. To remove a plant from your list, simply click it again.</p>
+                <Search onSearch={this.handleSearch} />
             <PlantList handleClick={this.handleEvent} plants={this.state.searchPlants} />
             </div>
-            <div id="selected-plants">
+            <div id="selected-plants" className="plants">
             <PlantList handleClick={this.handleEvent} plants={this.state.selectedPlants} />
             </div>
+
+            <img src={placeholder} style={{ width: "600px" }} />
         </div>
         )
     }   
@@ -80,6 +106,24 @@ const PlantList = (props: {plants: Array<PlantProps>, handleClick(plant: PlantPr
         <Plant plant={item} handleClick={props.handleClick} />
     )}
     </ul>)
+}
+
+type SearchProps = {
+    onSearch: () => void,
+}
+class Search extends React.Component<SearchProps, {}> {
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div id="search">
+                <input id="search-input" type="text"></input> <button onClick={this.props.onSearch}>Submit</button>
+            </div>
+        )
+    }
 }
 
 interface PlantDisProps {
