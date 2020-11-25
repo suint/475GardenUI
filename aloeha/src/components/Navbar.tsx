847e04fb6c7811d9fc44ab7b7f8b1122e67fafdd
suint <kellymi@udel.edu>
2020-11-25 18:36:30 -0500

    added back and next to navbar

--- aloeha/src/components/Navbar.tsx
+++ aloeha/src/components/Navbar.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { Link, withRouter, useLocation } from "react-router-dom";
+ 
+const pages = ["/", "/select", "/questionnaire", "/design", "/preview", "/build"];
 
 function Navbar(props: any) {
+    const location = useLocation();
+    const pageIndex = pages.findIndex(element => element === location.pathname);
+    const prevPage = pages[pageIndex-1];
+    const nextPage = pages[pageIndex+1];
   return (
-    <div className="navigation">
+    <div className="row">
+        {prevPage ? 
+        <Link className="nav-link" to={prevPage}>
+            Back
+        </Link> : <span></span>}
         <Link className="nav-link" to="/">
             Home
         </Link>
@@ -22,6 +32,10 @@ function Navbar(props: any) {
         <Link className="nav-link" to={"/build"}>
             Build
         </Link>
+        {nextPage ? 
+        <Link className="nav-link" to={nextPage}>
+            Next
+        </Link> : <span></span>}
     </div>
   );
 } 
