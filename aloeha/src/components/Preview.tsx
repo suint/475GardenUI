868209929fe89a7d924ab22587020d54b9a41f1d
suint <kellymi@udel.edu>
2020-12-12 17:14:32 -0500

    preview page loads objects

--- aloeha/src/components/Preview.tsx
+++ aloeha/src/components/Preview.tsx
@@ -1,13 +1,56 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import Draggable, {DraggableBounds} from 'react-draggable';
 import placeholder from "./img/reference pictures/Screen Shot 2020-10-18 at 10.27.30 PM.png"
 
-function Preview(props: any) {
+type PreviewProps = {
+    gardenObjects: GardenObject[],
+    plants: Plant[]
+}
+
+type PreviewState = {
+    year: number,
+    season: string,
+    viewMode: string,
+}
+
+class Preview extends React.Component<any, PreviewState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            year: 0,
+            season: "Spring",
+            viewMode: "topdown"
+        }
+    }
+    render() {
+        return (
+            <div id="design">
+                <div id="layout">
+                    {this.props.gardenObjects.map((object: GardenObject) => {
+                        return <ObjectDisplay gardenObject={object} viewMode={this.state.viewMode}/>})}
+                </div>
+                <img src={placeholder} style={{ width: "600px" }} />
+            </div>
+        )
+    }
+}
+
+const ObjectDisplay = (props: {gardenObject: GardenObject, viewMode: string}) => {
+    let { x, y } = props.gardenObject;
+    let bounds: DraggableBounds = {top: 0, bottom: 600, left: 0, right: 500};
+    if (props.viewMode == "window") {
+        bounds.top = 300;
+        y *= .6;
+    }
     return (
-        <div id="design">
-            <h1>Preview widget goes here</h1>
-            <img src={placeholder} style={{ width: "600px" }} />
-        </div>
+        <Draggable defaultPosition={{x: x, y: y}} bounds={bounds} disabled={true}> 
+            <div className="garden-obj">
+            <img src={props.gardenObject.image}></img>
+                    <p>{x}, {y}</p>
+                    <p>{props.gardenObject.name}</p>
+            </div>
+        </Draggable>
     )
 }
 
