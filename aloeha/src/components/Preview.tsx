b23f4d11bd0037bfe30bf759f702b1db226d58e4
suint <kellymi@udel.edu>
2020-12-12 20:18:00 -0500

    preview changes based on window or top down, info panel added

--- aloeha/src/components/Preview.tsx
+++ aloeha/src/components/Preview.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import Draggable, {DraggableBounds} from 'react-draggable';
 import placeholder from "./img/reference pictures/Screen Shot 2020-10-18 at 10.27.30 PM.png"
+import { transcode } from "buffer";
+import { transform } from "lodash";
 
 type PreviewProps = {
     gardenObjects: GardenObject[],
@@ -20,17 +22,30 @@ class Preview extends React.Component<any, PreviewState> {
         this.state = {
             year: 0,
             season: "Spring",
-            viewMode: "topdown"
+            viewMode: "Top-down"
+        }
+        this.toggleViewMode = this.toggleViewMode.bind(this);
+    }
+
+    toggleViewMode = () => {
+        if (this.state.viewMode == "Top-down") {
+            this.setState({viewMode: "Window"});
+        } else {
+            this.setState({viewMode: "Top-down"})
         }
     }
+
     render() {
         return (
             <div id="design">
+            <button onClick={this.toggleViewMode}>{this.state.viewMode}</button>
                 <div id="layout">
                     {this.props.gardenObjects.map((object: GardenObject) => {
                         return <ObjectDisplay gardenObject={object} viewMode={this.state.viewMode}/>})}
                 </div>
-                <img src={placeholder} style={{ width: "600px" }} />
+                <div id="info">
+                    gjsdfngsdf
+                </div>
             </div>
         )
     }
@@ -39,16 +54,21 @@ class Preview extends React.Component<any, PreviewState> {
 const ObjectDisplay = (props: {gardenObject: GardenObject, viewMode: string}) => {
     let { x, y } = props.gardenObject;
     let bounds: DraggableBounds = {top: 0, bottom: 600, left: 0, right: 500};
-    if (props.viewMode == "window") {
-        bounds.top = 300;
-        y *= .6;
+    let offset = {x: 0, y: 0};
+    let imgTransform = 1;
+    if (props.viewMode == "Window") {
+        offset.y = 250;
+        imgTransform = y/600 + .5;
+        y = (y/600) * 250; // new y bound is half of screen
     }
+    let imgTransformStyle = "scale(" + imgTransform + ", " + imgTransform + ")";
     return (
-        <Draggable defaultPosition={{x: x, y: y}} bounds={bounds} disabled={true}> 
+        <Draggable position={{x: x, y: y}} positionOffset={offset} bounds={bounds} disabled={true}> 
             <div className="garden-obj">
-            <img src={props.gardenObject.image}></img>
+            <img src={props.gardenObject.image} style={{"transform": imgTransformStyle}}></img>
                     <p>{x}, {y}</p>
                     <p>{props.gardenObject.name}</p>
+                    <p>{props.viewMode}</p>
             </div>
         </Draggable>
     )
