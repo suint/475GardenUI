bcc37a13c072700a8a73e0845a73667e315d0fe0
suint <kellymi@udel.edu>
2020-12-14 15:43:12 -0500

    fixed button on homepage

--- aloeha/src/components/Start.tsx
+++ aloeha/src/components/Start.tsx
@@ -9,9 +9,8 @@ function Start(props: any) {
             <h1>Aloe-Ha</h1>
             <img src={FadeLogo} style={{"width": "300px"}}/>
             <h3>Find native plants for your garden</h3>
-            <button className="start-button">
-                <Link to="/select">Begin planning</Link>
-            </button>
+                <Link to="/select"><button className="start-button">Begin planning</button></Link>
+            
         </div>
     )
 }
