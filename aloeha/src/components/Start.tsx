99e1b0154784beff9f6b6b5a3ab0d554845614b0
suint <kellymi@udel.edu>
2020-12-14 15:05:58 -0500

    start page has logo and title

--- aloeha/src/components/Start.tsx
+++ aloeha/src/components/Start.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Fade } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
+import FadeLogo from "./img/fade_logo.png"
 
 function Start(props: any) {
     return (
         <div id="start">
+            <h1>Aloe-Ha</h1>
+            <img src={FadeLogo} style={{"width": "300px"}}/>
+            <h3>Find native plants for your garden</h3>
             <button className="start-button">
-                <Link to="/select">Start New Plot</Link>
+                <Link to="/select">Begin planning</Link>
             </button>
         </div>
     )
--- aloeha/src/typings/modules.d.ts
+++ aloeha/src/typings/modules.d.ts
@@ -1,4 +1,3 @@
 declare module "react-hover";
 declare module "react-responsive-carousel";
 declare module "react-responsive-accordion";
-declare module "react-favicon";
